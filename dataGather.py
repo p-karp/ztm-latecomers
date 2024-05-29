525e71f2661f29e9dcf5ec82fcefbbec29e99094
romateusz <161944813+romateusz@users.noreply.github.com>
2024-05-29 15:54:22 +0200

    Add files via upload
    Podstawa programu do pobierania danych

---
+++ dataGather.py
@@ -0,0 +1,67 @@
+import warsaw_data_api as wawztm
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import sys
+
+# Tworzenie obiektu API
+ztm = wawztm.ztm(apikey='7febd25f-aa4e-4c31-8b8a-d517530106ca')
+
+
+# Funkcja pobierająca dane o przystankach
+def collectstops():
+    '''
+    dodać strukturę z danymi przysanków
+    zapisać przysatnki w tej strukturze
+    '''
+    pass
+
+
+# Funkcja pobierające dane o autobusach
+def collectBusesData():
+    exp = True
+    while(exp):
+        try:
+            buses = ztm.get_buses_location()
+            exp = False
+        except TypeError:
+            sys.sleep(500)
+    return buses
+
+
+# Funkcja pobierające dane o tramwajach
+def collectTramsData():
+    exp = True
+    while(exp):
+        try:
+            trams = ztm.get_trams_location()
+            exp = False
+        except TypeError:
+            sys.sleep(500)
+    return trams
+
+
+# Funkcja obliczająca opóźnienie autobusu
+def calculateETA(veh):
+
+    '''dodać logikę'''
+    print("numer pojazdu ", veh.vehicle_number)
+    print("numer linii ", veh.lines)
+    print("szerokość geograficzna: ", veh.location.latitude)
+    print("długość geograficzna: ", veh.location.longitude)
+    print("---")
+
+def main():
+    stops = collectstops()
+    buses = collectBusesData()
+    trams = collectTramsData()
+
+# obliczanie ETA 
+    for bus in buses:
+        calculateETA(bus)
+    for tram in trams:
+        calculateETA(tram)
+
+    '''stworzyć pętlę '''
+if __name__ == "__main__":
+    main()
