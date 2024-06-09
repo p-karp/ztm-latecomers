8ab9ef7c0bac6e0db94644f6294e6fd094474820
p-karp <p.karp@student.uw.edu.pl>
2024-06-10 00:45:30 +0200

    Przeksztalcanie rozkladow jazdy i zaczecie glownej iteracji

--- dataGather.py
+++ dataGather.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 import busStops as bs
-
+from datetime import datetime
 
 # Funkcja pobierające dane o autobusach
-def collectBusesData(ztm):
+def collectBusesData(ztm, linia):
     exp = True
     while(exp):
         try:
-            buses = ztm.get_buses_location()
+            buses = ztm.get_buses_location(line = linia)
             exp = False
         except TypeError:
             time.sleep(2)
@@ -18,15 +18,26 @@ def collectBusesData(ztm):
 
 
 # Funkcja pobierające dane o tramwajach
-def collectTramsData(ztm):
+def collectTramsData(ztm, linia):
     exp = True
     while(exp):
         try:
-            trams = ztm.get_trams_location()
+            trams = ztm.get_trams_location(line = linia)
             exp = False
         except TypeError:
             time.sleep(2)
     return trams
+ 
+# Funkcja pobierające dane o rozkładach jazdy   
+def collectScheduleData(ztm, przystanek):
+    exp = True
+    while(exp):
+        try:
+            schedule = ztm.get_bus_stop_schedule_by_id(przystanek[0], przystanek[1], przystanek[2])
+            exp = False
+        except TypeError:
+            time.sleep(2)
+    return schedule
 
 
 # Funkcja obliczająca opóźnienie pojazdu
@@ -43,6 +54,17 @@ def calculateETA(veh):
     print("długość geograficzna: ", veh.location.longitude)
     print("---")
 
+'''
+Tutaj określamy które przystanki i linie bierzemy pod uwagę
+'''
+#przystanki = [("7009", "01", "182"), ("7009", "01", "143"), ("7002", "01", "517"), ("7002", "01", "N24")]
+przystanki = [("3032", "01", "N31"), ("3032", "01", "N81")]
+
+''' 
+Struktura opoznienia
+
+opoznienia = {"id_przystanku" : {"kierunek": {"nr_linii": {"nr_brygady" : {"godzina_odjazdu": ["min_odl", "czas_przyjazdu" ,"pozostale_proby"]}}}}}
+'''
 
 def main():
     # Tworzenie obiektu API
@@ -50,8 +72,33 @@ def main():
 
     # Funkcja pobierająca dane o przystankach
     stops = bs.getBusStopsData()
-    print(stops)
+    #print(stops)
+    
+    # Pobieranie rozkładów jazdy
+    ''' To trzeba będzie robić każdego dnia na nowo (bo zmiany w weekend) '''
+    
+    czas = datetime.now().strftime('%H:%M:%S')
 
+    opoznienia = {}
+    for przystanek in przystanki:
+        schedule = collectScheduleData(ztm, przystanek)
+        brygady = {}
+        for ride in schedule.rides:
+            if ride.time <= czas:
+                continue
+            if ride.brigade not in brygady:
+                brygady[ride.brigade] = {ride.time : [1000000, 0, 10]}
+            else:
+                brygady[ride.brigade][ride.time] = [1000000, 0, 10]
+        
+        if przystanek[0] not in opoznienia:
+            opoznienia[przystanek[0]] = {przystanek[1] : {przystanek[2] : brygady}}
+        else:
+            if przystanek[1] not in opoznienia[przystanek[0]]:
+                opoznienia[przystanek[0]][przystanek[1]] = {przystanek[2] : brygady}
+            else:
+                opoznienia[przystanek[0]][przystanek[1]][przystanek[2]] = brygady
+    print(opoznienia)
     '''Uwaga: To są wartości testowe!'''
     # startowy (aktualny) czas zbierania danych w sekundach
     t = 0
@@ -61,19 +108,44 @@ def main():
     # krok czasowy co ile są zbierane dane w sekundach
     ''' Docelowo minuta dt = 60 lub półminuty dt = 30'''
     dt = 10   
-
+    
+    limit_odl = 0.011
+    limit_czasu = 15 #w minutach
+    
+    
     # główna pętla
     while(t <= t_lim):
-        buses = collectBusesData(ztm)
-        trams = collectTramsData(ztm)
-
-        # obliczanie ETA 
+        
+        
+        #trams = collectTramsData(ztm)
+        
+        for przystanek in opoznienia:
+            for nr_przystanku in opoznienia[przystanek]:
+                p_nazwa, p_x, p_y = stops[przystanek][nr_przystanku]
+                for linia in opoznienia[przystanek][nr_przystanku]:
+                    buses = collectBusesData(ztm, linia)
+                    for brygada in opoznienia[przystanek][nr_przystanku][linia]:
+                        godz_planowa = list(opoznienia[przystanek][nr_przystanku][linia][brygada].keys())[0]
+                        godz_planowa_popr = f"{int(godz_planowa[:2])-24}{godz_planowa[2:]}" # TODO: NA POZIOMIE ZAPISYWANIA W OPOZNIENIACH (?)
+                        print(godz_planowa_popr)
+                        for b in buses:
+                            # TODO: aktualny - b.time < 3 minut żeby brać pod uwagę
+                            if(b.brigade == brygada):
+                                print(b.time)
+                        
+                        # TODO: zapisywanie do pliku
+                        #del opoznienia[przystanek][nr_przystanku][linia][brygada][godz_planowa]
+                        
+            
+        break
+        # obliczanie ETA
+        ''' 
         for bus in buses:
             calculateETA(bus)
         for tram in trams:
             calculateETA(tram)
-
-        print(t)
+        '''
+        # TODO: obliczyć kiedy wykonać następne zapytanie
         time.sleep(dt)
         t = t + dt
 
