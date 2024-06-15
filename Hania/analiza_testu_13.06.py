1db3723141336a10b5a844b5d7edaaff560d67a4
romateusz <romanmateusz15@gmail.com>
2024-06-15 13:30:29 +0200

    analiza, delemiter

--- Hania/analiza_testu_13.06.py
+++ Hania/analiza_testu_13.06.py
@@ -4,7 +4,7 @@ import scipy.stats as st
 import csv
 
 with open('test_13_06_2024.csv', 'r') as file:
-    csv_reader = csv.reader(file)
+    csv_reader = csv.reader(file, delimiter=';')
     
     for row in csv_reader:
         print(row)
