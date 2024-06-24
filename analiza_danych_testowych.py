6d4d3eab7aada6ca9b7ee9791c04f2c6e7701c50
romateusz <romanmateusz15@gmail.com>
2024-06-24 15:26:14 +0200

    ulepszenie analizy danych testowych - uwzględnienie wielu dni

--- analiza_danych_testowych.py
+++ analiza_danych_testowych.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 '''Zabawa'''
-df = pd.read_csv('./wyniki/22-06_15.csv', encoding='windows-1250')
+df = pd.read_csv('./wyniki/23-06_18:30.csv', encoding='windows-1250')
 
 # Filtrowanie rekordów, gdzie nazwa przystanku zaczyna się na literę "C"
 filtered_df = df[df['przystanek'].str.startswith(('C', 'm'), na=False)]
@@ -23,60 +23,73 @@ for index, row in df.iterrows():
     if values[2] in St.keys():
         if values[0] in St[values[2]].keys():
             if values[1] in St[values[2]][values[0]].keys():
-                St[values[2]][values[0]][values[1]].append(values[4:])
+                if values[4] in St[values[2]][values[0]][values[1]].keys():
+                    St[values[2]][values[0]][values[1]][values[4]].append(values[5])
+                else: 
+                    St[values[2]][values[0]][values[1]][values[4]] = [values[5]]
             else:
-                St[values[2]][values[0]][values[1]] = [values[4:]]
+                St[values[2]][values[0]][values[1]] = {values[4]: [values[5]]}
         else:
-            St[values[2]][values[0]] = {values[1]: [values[4:]]}
+            St[values[2]][values[0]] = {values[1]: {values[4]: [values[5]]}}
     else: 
-        St[values[2]] = {values[0]: {values[1]: [values[4:]]}}
+        St[values[2]] = {values[0]: {values[1]:{values[4]: [values[5]]}}}
 
 
 '''Pokaz jak to wygląda w środku'''
-# print(St[114]["Centrum Olimpijskie"][2])
+#print(St[162])
 
-
-df = pd.read_csv('./busSchedules.csv', delimiter=";", encoding='windows-1250')
-for index, row in df.iterrows():
-    if(index > 28 and index < 40):
-        values = row.values.tolist()
-        St[values[2]][values[0]][values[1]][index-29].append(values[4])
-
-
-# Uważam, że fajniej tak analizować:
-scheduledArr = []
-df = pd.read_csv('./busSchedules.csv', delimiter=";", encoding='windows-1250')
-for index, row in df.iterrows():
-    if(index > 28 and index < 40):
-        values = row.values.tolist()
-        scheduledArr.append(values[4])
-
-def statystyki(scheduledArr):
+def statystyki(scheduledArr, file):
     scheduledDeltaArr = []
+    longDelays = 0
     for i in range(len(scheduledArr)-2):
         time1 = datetime.strptime(scheduledArr[i], '%H:%M:%S').time()
         time2 = datetime.strptime(scheduledArr[i+1], '%H:%M:%S').time()
         time_diff = datetime.combine(datetime.min, time2) - datetime.combine(datetime.min, time1)
         minutes_diff = time_diff.total_seconds() / 60.0
-        if(minutes_diff < 30): scheduledDeltaArr.append(minutes_diff)
+        scheduledDeltaArr.append(minutes_diff)
 
-    # Obliczanie interesujących nas statystyk
-    mean_value = np.mean(scheduledDeltaArr)
-    std_deviation = np.std(scheduledDeltaArr)
-    min_value = np.min(scheduledDeltaArr)
-    max_value = np.max(scheduledDeltaArr)
+    try:
+        # Obliczanie interesujących nas statystyk
+        mean_value = np.mean(scheduledDeltaArr)
+        std_deviation = np.std(scheduledDeltaArr)
+        min_value = np.min(scheduledDeltaArr)
+        max_value = np.max(scheduledDeltaArr)    
+    except:
+        print("Nie zarejestrowano żadnych przyjazdów dla tej lini")
 
-    print(f"Średnia: {mean_value}")
-    print(f"Odchylenie standardowe: {std_deviation}")
-    print(f"Minimum: {min_value}")
-    print(f"Maksimum: {max_value}\n")
+    finalScheduledDeltaArr = []
+    for delta in scheduledDeltaArr:
+        if(delta < 1.3 * mean_value): finalScheduledDeltaArr.append(delta)
+        else: longDelays += 1
 
-print("Wartości oczekiwane:")
-statystyki(scheduledArr)
+    try:
+        mean_value = np.mean(finalScheduledDeltaArr)
+        std_deviation = np.std(finalScheduledDeltaArr)
+        min_value = np.min(finalScheduledDeltaArr)
+        max_value = np.max(finalScheduledDeltaArr)
+        file.write(f"Liczba wszystkich rejestracji:{len(scheduledDeltaArr)+2}\n")
+        file.write(f"Liczba dużych opóźnień: {longDelays}\n")
+        #print(f"Liczba dużych opóźnień: {longDelays}")
+        file.write(f"Średnia: {mean_value}\n")
+        #print(f"Średnia: {mean_value}")
+        file.write(f"Odchylenie standardowe: {std_deviation}\n")
+        #print(f"Odchylenie standardowe: {std_deviation}")
+        file.write(f"Minimum: {min_value}\n")
+        #print(f"Minimum: {min_value}")
+        file.write(f"Maksimum: {max_value}\n\n")
+        #print(f"Maksimum: {max_value}\n")
+    except:
+        file.write("Nie zarejestrowano żadnych przyjazdów dla tej lini\n\n")
+        #print("Nie zarejestrowano żadnych przyjazdów dla tej lini\n")
 
-scheduledArr = []
-for i in range(len(St[114]["Centrum Olimpijskie"][2])):
-    scheduledArr.append(St[114]["Centrum Olimpijskie"][2][i][1])
 
-print("Wartości faktyczne:")
-statystyki(scheduledArr)
+with open("analiza_danych_testowych.csv", "w") as file:
+    for line in St.keys():
+        for stop in St[line].keys():
+            scheduledArr = []
+            for nr in St[line][stop].keys():
+                for date in St[line][stop][nr].keys():
+                    scheduledArr = sorted(St[line][stop][nr][date])
+                    #print(f"Wartości faktyczne linia:{line} przystanek:{stop} nr:{nr} dzień:{date}")
+                    file.write(f"Wartości faktyczne linia:{line} przystanek:{stop} nr:{nr} dzień:{date}\n")
+                    statystyki(scheduledArr, file)
