72e042908d1337651ffd17ac06df9d788e6640b5
romateusz <romanmateusz15@gmail.com>
2024-06-28 00:07:20 +0200

    Dodano codzienny wyzwalacz run.py

---
+++ run.py
@@ -0,0 +1,29 @@
+import schedule
+import time
+import subprocess
+
+# Global variable to keep the process reference
+process = None
+
+def start_collect_arrivals():
+    global process
+    if process is None:
+        print("Starting collectArrivals.py")
+        process = subprocess.Popen(["python3", "collectArrivals.py"])
+
+def stop_collect_arrivals():
+    global process
+    if process is not None:
+        print("Stopping collectArrivals.py")
+        # Terminate the process
+        process.terminate()
+        process.wait()
+        process = None
+
+# Schedule tasks
+schedule.every().day.at("03:00").do(start_collect_arrivals)
+schedule.every().day.at("01:00").do(stop_collect_arrivals)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
