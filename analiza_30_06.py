bcbe1d5e4d27fa9c4ac3e5325f9eb1cec7d74206
habadabadabdab <h.moczek@student.uw.edu.pl>
2024-07-06 14:49:07 +0200

    więcej analizy

--- analiza_30_06.py
+++ analiza_30_06.py
@@ -5,26 +5,21 @@ from datetime import datetime
 
 
 D, S = pr.processing('./zebraneDane/dane_ok/rj_30-06-beta.csv', './zebraneDane/dane_ok/dane_30-06.csv')
-print(D[500])
-print(S[500])
+
 
 czas_przyjazdu = D[523]['Saska'][1]['2024-06-30']
 czas_rozkłady = S[523]['Saska'][1]['2024-06-30']
 
 def przystanki(czas_przyjazdu, czas_rozkłady):
     od_przystanku= []
-    now = datetime.now()
-    for i in czas_przyjazdu:
+    for i in czas_rozkłady:
         pom = []
-        for j in czas_rozkłady:
-            time1 = datetime.strptime(i, '%H:%M:%S')
-            time2 = datetime.strptime(j, '%H:%M:%S')
-            #time1_full = datetime.combine(datetime.min, time1)
-            #time2_full = datetime.combine(datetime.min, time2)
+        for j in czas_przyjazdu:
+            time1 = datetime.strptime(j, '%H:%M:%S')
+            time2 = datetime.strptime(i, '%H:%M:%S')
             time_diff =  time1 - time2
             minutes_diff = time_diff.total_seconds() / 60.0
             time_do_listy = np.abs(minutes_diff)
-            
             pom.append(time_do_listy)
         od_przystanku.append(np.min(pom))
     
@@ -32,7 +27,6 @@ def przystanki(czas_przyjazdu, czas_rozkłady):
 
 
 a = przystanki(czas_przyjazdu, czas_rozkłady)
-
 
 def od_przystanku(przyjazd, rozkład):
     przystanek = {}
@@ -47,11 +41,22 @@ def od_przystanku(przyjazd, rozkład):
 
 
 b=od_przystanku(D, S)
-plt.plot(b.keys(), b.values())                    
+                 
 
         
-        
- 
+def od_dnia(przyjazd, rozkład):
+    przystanek = {}
+    for line in przyjazd.keys():
+        for stop in przyjazd[line].keys():
+            for nr in przyjazd[line][stop].keys():
+                for date in przyjazd[line][stop][nr].keys():
+                    przystanek[date]= przystanki(przyjazd[line][stop][nr][date], rozkład[line][stop][nr][date])
+
+
+    return przystanek
+
+c = od_dnia(D, S)
+print(c)
 
 
         
