5f01bec3ccfd589dab81ac3af9aa586c869274d5
romateusz <romanmateusz15@gmail.com>
2024-07-06 12:30:03 +0200

    Dodanie danych z 05-07

--- zebraneDane/usuwanieLiniizPrefixem.py
+++ zebraneDane/usuwanieLiniizPrefixem.py
@@ -1,6 +1,6 @@
 def remove_lines_starting_with(prefix):
     for d in ["29-06", "30-06", "01-07", "02-07", "04-07", "05-07"]:
-        with open("dane_" + d + ".csv", 'r', encoding='windows-1250') as file:
+        with open("./orginalne/dane_" + d + ".csv", 'r', encoding='windows-1250') as file:
             lines = file.readlines()
 
         with open("./dane_ok/dane_" + d + ".csv", 'w', encoding='windows-1250') as file:
